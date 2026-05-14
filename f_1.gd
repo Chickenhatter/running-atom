98d048fc0b5516e5d95bfd45208b2b2c71266dad
Chickenhatter <b3rr1350bra1n@gmail.com>
2026-05-14 22:26:14 +1200

    I am dumb
    I just realized instead of this complex system I could just move character to node 2d attached. Also previous commit was for accidentally deleting windows on one and trying to solved where commits are saved

--- f_1.gd
+++ f_1.gd
@@ -3,29 +3,20 @@ var first = 0
 var second = 0
 var zim = false
 func _ready() -> void:
-	velocity = Vector2(10, 10).normalized() * 200 a
+	velocity = Vector2(10, 10).normalized() * 200
 
 func _physics_process(delta: float) -> void:
 	var collide = move_and_collide(velocity * delta)
 	if collide:
 		velocity = velocity.bounce(collide.get_normal())
 	if zim == true:
-		$"../../../Character/Node2D/main_hydrogen".position = $".".position
-		
+		$"../../../Character/Node2D/main_hydrogen".global_position = $CollisionPolygon2D/Node2D.global_position
+	else:
+		$connect_f_1.rotation_degrees += 5
 
 
 func _on_hydrogen_main_area_entered(area: Area2D) -> void:
 	if area.name == 'connect_f_1':
 		if first == 0:
-			first = $"../../../Character/Node2D/main_hydrogen".position.x
-			first -= $".".position.x
-			first -=$"../../../Character/Node2D/main_hydrogen".position.x
-			second = $"../../../Character/Node2D/main_hydrogen".position.y
-			second -= $".".position.x 
-			print(second)
-			print(first)
-			$"../../../Node2D".position.x = first
-			$"../../../Node2D".position.y = second
-			$"../../../Character/Node2D".position.x = first
-			$"../../../Character/Node2D".position.y = second
+			$"../../../Character/Node2D/main_hydrogen".position = Vector2(0,0)
 			zim = true
